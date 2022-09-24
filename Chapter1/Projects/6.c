d6f9f0f18b5a6f6019666db900133c5d506b1777
Pedro Henrique <58053848+Gwynbleidd-gH@users.noreply.github.com>
2022-09-23 21:12:59 -0300

    Create 6.c

---
+++ Chapter1/Projects/6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+  #define MESSAGE_MAX_LEN 100
+	char message[MESSAGE_MAX_LEN];
+	int i;
+	
+	fgets(message, MESSAGE_MAX_LEN, stdin);
+	message[strlen(message) - 1] = '\0';
+	for (i = 0; message[i] != '\0'; i++) {
+		switch (message[i]) {
+		case 'a': case 'A':
+			printf("4");
+			break;
+		case 'b': case 'B':
+			printf("8");
+			break;
+		case 'e': case 'E':
+			printf("3");
+			break;
+		case 'i': case 'I':
+			printf("1");
+			break;
+		case 'o': case 'O':
+			printf("0");
+			break;
+		case 's': case 'S':
+			printf("5");
+			break;
+		default:
+			if (message[i] >= 'a' && message[i] <= 'z')
+				printf("%c", message[i] - ('a' - 'A'));
+			else
+				printf("%c", message[i]);
+		}
+	}
+  printf("!!!!!!!!!!\n");
+  return 0;
+}
