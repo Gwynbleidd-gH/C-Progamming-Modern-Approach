a84d26701cc8f09c13ec3fa82af638b11b78b84e
Pedro Henrique <58053848+Gwynbleidd-gH@users.noreply.github.com>
2022-10-12 23:43:14 -0300

    Create 9.c

---
+++ Chapter1/Projects/9.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+#include <stdbool.h>
+
+int main(void)
+{
+  #define N 10
+	char board[N][N];
+	char letter = 'A';
+	int random_num;
+	int i, j;
+	int row = 0;
+	int col = 0;
+	bool up_blocked = false;
+	bool down_blocked = false;
+	bool left_blocked = false;
+	bool right_blocked = false;
+	bool moved = false;
+
+	/* fills the board with dots . */
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			board[i][j] = '.';
+	srand(time(NULL));
+
+	board[0][0] = letter++;
+	while (letter <= 'Z') {
+		random_num = rand() % 4;
+		switch (random_num) {
+		case 0:
+			if ((!up_blocked && row > 0) && board[row - 1][col] == '.') {
+				board[--row][col] = letter++;
+				moved = true;
+			}
+			else
+				up_blocked = true;
+			break;
+		case 1:
+			if ((!down_blocked && row < N - 1) && board[row + 1][col] == '.') {
+				board[++row][col] = letter++;
+				moved = true;
+			}
+			else
+				down_blocked = true;
+			break;
+		case 2:
+			if ((!left_blocked && col > 0) && board[row][col - 1] == '.') {
+				board[row][--col] = letter++;
+				moved = true;
+			}
+			else
+				left_blocked = true;
+			break;
+		case 3:
+			if ((!right_blocked && col < N - 1) && board[row][col + 1] == '.') {
+				board[row][++col] = letter++;
+				moved = true;
+			}
+			else
+				right_blocked = true;
+			break;
+		}
+		if (moved) {
+			up_blocked = down_blocked = left_blocked = right_blocked = false;
+			moved = false;
+		}
+		if (up_blocked && down_blocked && left_blocked && right_blocked)
+			break;
+	}
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; ++j) {
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+    return 0;
+}
