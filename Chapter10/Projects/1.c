9350613aacd9c22192add96e53072aca5d620763
Pedro Henrique <58053848+Gwynbleidd-gH@users.noreply.github.com>
2022-12-25 21:20:33 -0300

    Update 1.c

--- Chapter10/Projects/1.c
+++ Chapter10/Projects/1.c
@@ -1,75 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define STACK_MAX_SIZE 100
 
 char stack[STACK_MAX_SIZE];
 
-int top = 0;
-
-bool is_full(void);
+int stack_top = 0;
 
 bool is_empty(void);
 
-void push(char ch);
+bool is_full(void);
+
+void push(char value);
 
 char pop(void);
 
+bool bem_formada(char str[]);
+
 int main(void)
 {
-    int ch;
+    char str[10];
 
     printf("Enter parentheses and/or braces: ");
-    while ((ch = getchar()) != '\n') {
-        if ((ch == ')' || ch == '}') && is_empty()) {
-            push(ch);
-            break;
-        }
-        else if (ch == '(' || ch == '{')
-            push(ch);
-        else if (ch == ')' && pop() != '(')
-            break;
-        else if (ch == '}' && pop() != '{')
-            break;
-    }
+    fgets(str, sizeof(str), stdin);
+    str[strlen(str) - 1] = '\0';
 
-    if (is_empty())
-        printf("Parentheses/braces are nested properly\n");
+    if (bem_formada(str))
+        printf("parentheses/braces are nested properly\n");
     else
-        printf("Parentheses/braces are not nested properly\n");
-    
-    return 0;
-}
+        printf("arentheses/braces are not nested properly\n");
 
-bool is_full(void)
-{
-    return top == STACK_MAX_SIZE;
+    return 0;
 }
 
 bool is_empty(void)
 {
-    return top == 0;
+    return stack_top == 0;
 }
 
-void push(char ch)
+bool is_full(void)
 {
-    if (is_full()) {
+    return stack_top == STACK_MAX_SIZE;
+}
+
+void push(char value)
+{
+    if (!is_full())
+        stack[stack_top++] = value;
+    else {
         printf("Stack Overflow!\n");
         exit(EXIT_FAILURE);
-    }
-    else {
-        stack[top++] = ch;
-    }
+    }  
 }
 
 char pop(void)
 {
-    if (is_empty()) {
-        printf("Stack Underflow!\n");
+    if (!is_empty())
+        return stack[--stack_top];
+    else {
+        printf("Stack Underflow\n");
         exit(EXIT_FAILURE);
     }
-    else {
-        return stack[--top];
+}
+
+bool bem_formada(char str[])
+{
+    int i;
+    char ch;
+
+    for (i = 0; str[i] != '\0'; i++) {
+        switch (str[i]) {
+        case ')':
+            if (is_empty() || (ch = pop()) != '(')
+                return false;
+            break;
+        case '}':
+            if (is_empty() || (ch = pop()) != '{')
+                return false;
+            break;
+        case '(': case '{':
+            push(str[i]);
+            break;
+        default:
+            return false;
+        }
     }
+
+    return is_empty();
 }
