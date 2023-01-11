046389eaa1c4b976c68a87bd7d5b4da586b3b1ed
Pedro Henrique <58053848+Gwynbleidd-gH@users.noreply.github.com>
2023-01-11 16:45:12 -0300

    Create 7.c

---
+++ Chapter12/Projects/7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+void max_min(int *a, int n, int *max, int *min);
+
+int main(void)
+{
+	int arr[5] = { 3, 5, 2, 1, 8 };
+	int max, min;
+
+	max_min(arr, 5, &max, &min);
+	printf("%d %d\n", max, min);
+
+	return 0;
+}
+
+void max_min(int *a, int n, int *max, int *min)
+{
+	int *ptr;
+
+	*max = *min = *a;
+	for (ptr = a; ptr < a + n; ptr++) {
+		if (*ptr > *max)
+			*max = *ptr;
+		else if (*ptr < *min)
+			*min = *ptr;
+	}
+}
